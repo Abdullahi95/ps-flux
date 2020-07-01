f2670dcd9cc88f51d554e9eceadffaad9b7bd3d8
Hassan822 <Hassan931221@gmail.com>
2020-07-01 17:32:51 +0100

    added aboutpage and homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Header</h1>
+      <p>Paragraph</p>
+      <a href="src\components\AboutPage.js">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
