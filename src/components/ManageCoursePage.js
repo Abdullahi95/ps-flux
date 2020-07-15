03395c10c05094ed935d7741d3cb566ed34b58a6
Hassan822 <Hassan931221@gmail.com>
2020-07-15 11:02:18 +0100

    added notification to app

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
+import * as CourseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
   const [course, setCourse] = useState({
@@ -19,10 +21,22 @@ const ManageCoursePage = (props) => {
     setCourse(updatedCourse);
   }
 
+  function handleSumbit(event) {
+    event.preventDefault();
+    CourseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast("Course Added");
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <CourseForm course={course} onChange={handleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSumbit}
+      />
     </>
   );
 };
